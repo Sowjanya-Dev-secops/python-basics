65b136df3ddcdd17e69913faec203e2527182100
sowjanya <sowji.muppala@gmail.com>
2026-01-06 17:44:23 +0530

    python

---
+++ 00-hello.py
@@ -0,0 +1,18 @@
+print("hello world")
+num_servers = 10
+print(num_servers)
+num_servers = 20 
+print(num_servers)
+
+is_prod = False 
+print(type(is_prod))
+
+a = 20
+b = 10
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print(div)
+floor_div = a // b
+print(floor_div)
